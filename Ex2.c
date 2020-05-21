06c86d56333b50f347ff0aca8de115448c5f90c3
MadMegaMouse <61424628+MadMegaMouse@users.noreply.github.com>
2020-05-21 16:16:48 +0300

    Update Ex2.c

--- Ex2.c
+++ Ex2.c
@@ -8,9 +8,7 @@ int main( int argc, char *argv[] )  {
 	T = 0;
 	N = atoi(n); // char * to int
 	D = atoi(d); // char * to int 
-	for (int i = 0; i < N+1; i++){
-		T = T + i * D;
-	}
+	for (int i = 0; i < N+1; i++){T = T + i * D;}
 	printf("The total number of spans: %i \n", N);
 	printf("The distance between the spans: %i \n", D);
 	printf("Total lenght of cables: %i \n", T);
